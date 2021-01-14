c74da7b101e72cc4c819d4d57db9ee8ca49b50e4
Junifar Hidayat <junifar.hidayat@dhanapala.id>
2021-01-14 19:51:55 +0700

    add validator

--- internal/app/usecase/authorization/impl/init.go
+++ internal/app/usecase/authorization/impl/init.go
@@ -5,20 +5,31 @@ import (
 	"github.com/beego-pos/internal/app/usecase/authorization"
 	"github.com/beego-pos/models"
 	"github.com/beego/beego/v2/core/logs"
+	"github.com/beego/beego/v2/core/validation"
 	"github.com/beego/beego/v2/server/web/context"
 )
-
 
 type AuthorizationImpl struct {
 	User user.IUser
 }
 
-func New(userRP user.IUser) authorization.IAuthorization{
+func New(userRP user.IUser) authorization.IAuthorization {
 	return &AuthorizationImpl{User: userRP}
 }
 
 func (impl *AuthorizationImpl) Registration(ctx *context.Context, user models.User) error {
 	logs.Debug("Test Dulu", 123)
+
+	validate := validation.Validation{}
+	validate.Required(user.Email, "emailRequired").Message("email tidak boleh kosong")
+	validate.Email(user.Email, "emailValid").Message("email harus valid")
+
+	if validate.HasErrors() {
+		for _, err := range validate.Errors {
+			logs.Error("error : ", err.Message)
+		}
+	}
+
 	err := impl.User.SetUser(ctx, user)
 	if err != nil {
 		return err
